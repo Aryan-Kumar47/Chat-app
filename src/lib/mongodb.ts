b6535c3e609057e9f15908be1cc648f5eeccacc3
Aryan-Kumar47 <kumarayan1012@gmail.com>
2024-03-21 15:33:39 +0530

    fixing bug

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -4,7 +4,6 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
 
-const uri = process.env.MONGODB_URI!
 const options = {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,12 +21,12 @@ if (process.env.NODE_ENV === "development") {
     _mongoClientPromise: Promise<MongoClient>
   }
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options)
+    client = new MongoClient(process.env.MONGODB_URI!, options)
     globalWithMongo._mongoClientPromise = client.connect()
   }
   clientPromise = globalWithMongo._mongoClientPromise
 } else {
-  client = new MongoClient(uri, options)
+  client = new MongoClient(process.env.MONGODB_URI!, options)
   clientPromise = client.connect()
 }
 
